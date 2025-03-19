f6788caf9bdf9af782a3ac84221ef1efcf8fa5df
tom <minajetty@gmail.com>
2025-03-19 23:31:24 +0100

    add springdoc openAPI for define your own interfaces

--- printingsvc/src/main/java/com/verti/printing/controller/PrintingController.java
+++ printingsvc/src/main/java/com/verti/printing/controller/PrintingController.java
@@ -1,12 +1,15 @@
 package com.verti.printing.controller;
 
 import com.verti.printing.service.PrintingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Printing API", description = "API for printing services")
 public class PrintingController {
 
     private final PrintingService printingService;
@@ -15,6 +18,7 @@ public class PrintingController {
         this.printingService = svc;
     }
 
+    @Operation(summary = "Health Check", description = "Returns the health status of the application")
     @GetMapping("/healthz")
     public String healthCheck() {
         return printingService.healthCheck();
